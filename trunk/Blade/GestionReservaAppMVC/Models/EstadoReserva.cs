3352d9950a4f59df801a2edb03cfbc8f057fb927
lljecamayoll@gmail.com <lljecamayoll@gmail.com@425217d3-b716-0fd7-7625-4c16d5886a98>
2013-09-16 04:05:28 +0000

    - Modulo Confirmar Reserva

---
+++ trunk/Blade/GestionReservaAppMVC/Models/EstadoReserva.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionReservaAppMVC.Models
+{
+    public class EstadoReserva
+    {
+        public int codEstado { get; set; }
+        public string desEstado { get; set; }
+
+    }
+}
---
+++ trunk/Blade/GestionReservaAppMVC/Models/Reserva.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionReservaAppMVC.Models
+{
+    public class Reserva
+    {
+        public string CodReserva { get; set; }
+          public Sede Sede { get; set; }
+          public EspacioDeportivo EspacioDeportivo { get; set; }
+          public Actividad Actividad { get; set; }
+          public  Usuario Usuario { get; set; }
+         public DateTime FechaReserva { get; set; }
+         public string DiaCorto { get; set; }
+         public string HoraInicio { get; set; }
+         public string HoraTermino { get; set; }
+         public EstadoReserva Estado { get; set; }
+         
+          
+    }
+}

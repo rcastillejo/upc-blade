e0f8e453fd30f6c0504820cc82cdee1d6dae8475
maritzacalderonf@gmail.com <maritzacalderonf@gmail.com@425217d3-b716-0fd7-7625-4c16d5886a98>
2013-09-17 16:44:56 +0000

    Carga las fuentes del registro de reglas para un espacio deportivo

---
+++ trunk/Blade/GestionReservaAppMVC/Models/ReglaEspacioDeportivo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionReservaAppMVC.Models
+{
+    public class ReglaEspacioDeportivo
+    {
+        public EspacioDeportivo EspacioDeportivo { get; set; }
+        public int MinAnticipacionReservarMinuto{ get; set; }
+        public int MaxAnticipacionReservaDia { get; set; }
+        public int MinAnticipacionCancelarHora { get; set; }
+        public int MaxHoraReservaDia { get; set; }
+        public int MaxHoraReservaSemana { get; set; }
+        public int MaxNumUsuario{ get; set; }
+    }
+}

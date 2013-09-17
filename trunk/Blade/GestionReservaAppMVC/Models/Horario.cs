e20efb3ed5fb1ca663b9ddbe31be2d88710ad951
jjdelacru28@gmail.com <jjdelacru28@gmail.com@425217d3-b716-0fd7-7625-4c16d5886a98>
2013-09-17 05:57:53 +0000

    Modelo Horario

---
+++ trunk/Blade/GestionReservaAppMVC/Models/Horario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionReservaAppMVC.Models
+{
+    public class Horario
+    {
+        public int Codigo { get; set; }
+        public EspacioDeportivo EspacioDeportivo { get; set; }
+        public Sede  Sede { get; set; }
+        public String  Dia { get; set; }
+        public String Horainicio { get; set; }
+        public String HoraFin { get; set; }
+     }
+}

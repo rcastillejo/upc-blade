bd6a9670c735d124d2420bd7e938b249a715fefd
jjdelacru28 <jjdelacru28@gmail.com>
2013-09-17 16:58:58 +0000

    Clase Detale de Horario

---
+++ Blade/GestionReservaAppMVC/Models/DetalleHorario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionReservaAppMVC.Models
+{
+    public class DetalleHorario
+    {
+        public Horario Horario { get; set; }
+        public String Dia { get; set; }
+        public String Horainicio { get; set; }
+        public String HoraFin { get; set; }
+    }
+}

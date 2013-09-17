55cc3f941acbc23541f45984bf918a16b93a5c28
ricardo.castillejo <ricardo.castillejo@gmail.com>
2013-09-17 08:02:57 +0000

    Se carga las fuentes del servicio Sede

---
+++ Blade/EspacioDeportivoServices/ISedeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using EspacioDeportivoServices.Dominio;
+
+namespace EspacioDeportivoServices
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "ISedeService" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface ISedeService
+    {
+        [OperationContract]
+        List<Sede> listar();
+    }
+}
---
+++ Blade/EspacioDeportivoServices/SedeService.svc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using EspacioDeportivoServices.Persistencia;
+
+namespace EspacioDeportivoServices
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "SedeService" en el código, en svc y en el archivo de configuración a la vez.
+    public class SedeService : ISedeService
+    {
+        private SedeDAO sedeDAO = new SedeDAO();
+
+        public List<Dominio.Sede> listar()
+        {
+            return (List<Dominio.Sede>)sedeDAO.ListarTodos().ToList();
+        }
+    }
+}

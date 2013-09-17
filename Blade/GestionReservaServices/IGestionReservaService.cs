93a99d39761a6c740a0fb6e48376c934e74e570b
ricardo.castillejo <ricardo.castillejo@gmail.com>
2013-09-17 23:08:13 +0000

    Se carga el servicio Gestion de Reserva

---
+++ Blade/GestionReservaServices/IGestionReservaService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GestionReservaServices
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IGestionReservaService" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IGestionReservaService
+    {
+        [OperationContract]
+        EspacioDeportivoWS.EspacioDeportivo obtenerEspacio(int codigo);
+        [OperationContract]
+        List<EspacioDeportivoWS.EspacioDeportivo> listarEspacio();
+        [OperationContract]
+        EspacioDeportivoWS.EspacioDeportivo crearEspacio(string nombre, int sede);
+        [OperationContract]
+        EspacioDeportivoWS.EspacioDeportivo actualizarEspacio(int codigo, string nombre, int sede);
+        [OperationContract]
+        void eliminarEspacio(int Codigo);
+
+
+        [OperationContract]
+        List<SedeWS.Sede> listarSede();
+    }
+}

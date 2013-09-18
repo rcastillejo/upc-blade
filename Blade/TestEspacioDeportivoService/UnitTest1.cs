a21ed2d080424f6962e588bc2af70e6ddc29abde
ricardo.castillejo <ricardo.castillejo@gmail.com>
2013-09-18 01:52:12 +0000

    Se carga las pruebas

--- Blade/TestEspacioDeportivoService/UnitTest1.cs
+++ Blade/TestEspacioDeportivoService/UnitTest1.cs
@@ -9,6 +9,17 @@ namespace TestEspacioDeportivoService
     [TestClass]
     public class UnitTest1
     {
+
+        [TestMethod]
+        public void TestCrear()
+        {
+            EspacioDeportivoWS.EspacioDeportivoServiceClient proxy = new EspacioDeportivoWS.EspacioDeportivoServiceClient();
+            EspacioDeportivoWS.EspacioDeportivo espacio = proxy.crear("prueba", 1);
+
+            Assert.AreEqual("prueba", espacio.Nombre);
+            Assert.AreEqual(1, espacio.Sede.Codigo);
+        }
+
         [TestMethod]
         public void TestObtener()
         {

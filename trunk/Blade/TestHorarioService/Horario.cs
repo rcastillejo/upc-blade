a6810724318f9611ffd6f6771edc7dcd8843dbf8
ricardo.castillejo@gmail.com <ricardo.castillejo@gmail.com@425217d3-b716-0fd7-7625-4c16d5886a98>
2013-10-01 23:25:30 +0000

    Se carga las pruebas para el servicio Horario

---
+++ trunk/Blade/TestHorarioService/Horario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace TestHorarioService
+{
+    public class Horario
+    {
+        public int Codigo { get; set; }
+        public String Dia { get; set; }
+        public String HoraInicio { get; set; }
+        public String HoraFin { get; set; }
+    }
+}

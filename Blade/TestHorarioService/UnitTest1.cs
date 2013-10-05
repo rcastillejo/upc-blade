b143574b5385e25da63f0eb19e436a02a9b5a083
ricardo.castillejo <ricardo.castillejo@gmail.com>
2013-10-05 15:58:49 +0000

    Se carga las pruebas de la 2da excepcion.

--- Blade/TestHorarioService/UnitTest1.cs
+++ Blade/TestHorarioService/UnitTest1.cs
@@ -16,8 +16,7 @@ namespace TestHorarioService
         [TestMethod]
         public void Crear()
         {
-            //string horarioJson = "{\"Codigo\":\"1\",\"Dia\":\"martes\",\"HoraInicio\":\"8:20\",\"HoraFin\":\"12:00\"}";
-            string horarioJson = "{\"Codigo\":1,\"Dia\":\"lunes\",\"HoraInicio\":\"08:15\",\"HoraFin\":\"15:00\"}";
+            string horarioJson = "{\"Codigo\":1,\"Dia\":\"jueves\",\"HoraInicio\":\"08:15\",\"HoraFin\":\"15:00\"}";
             byte[] data = Encoding.UTF8.GetBytes(horarioJson);
 
             HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://localhost:22057/Horarios.svc/Horarios");
@@ -31,26 +30,38 @@ namespace TestHorarioService
             var res = (HttpWebResponse)req.GetResponse();
             StreamReader reader = new StreamReader(res.GetResponseStream());
             string horarioObtenidoJson = reader.ReadToEnd();
-            JavaScriptSerializer js = new JavaScriptSerializer();
-            Horario horarioCreado = js.Deserialize<Horario>(horarioObtenidoJson);
 
-            Assert.AreEqual(1, horarioCreado.Codigo);
-            Assert.AreEqual("martes", horarioCreado.Dia);
+            Assert.AreEqual("\"El horario del espacio deportivo registrado exitosamente\"", horarioObtenidoJson);
         }
+
 
         [TestMethod]
-        public void Eliminar()
+        public void CrearError()
         {
+            try{
+            string horarioJson = "{\"Codigo\":1,\"Dia\":\"jueves\",\"HoraInicio\":\"08:00\",\"HoraFin\":\"15:00\"}";
+            byte[] data = Encoding.UTF8.GetBytes(horarioJson);
+            
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://localhost:22057/Horarios.svc/Horarios");
+            req.Method = "POST";
+            req.ContentLength = data.Length;
+            req.ContentType = "application/json";
+            
+            var reqStream = req.GetRequestStream();
+            reqStream.Write(data, 0, data.Length);
 
-            //HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://localhost:22057/Horarios.svc/Horarios/1/lunes");
-            //req.Method = "DELETE";
-            //req.ContentType = "application/json";
-            //var res = (HttpWebResponse)req.GetResponse();
-            //StreamReader reader = new StreamReader(res.GetResponseStream());
-            //string alumnoObtenidoJson = reader.ReadToEnd();
-            //Assert.AreEqual("", alumnoObtenidoJson);
+            var res = (HttpWebResponse)req.GetResponse();
+            StreamReader reader = new StreamReader(res.GetResponseStream());
+            string horarioObtenidoJson = reader.ReadToEnd();
+            
+            
+            }catch (WebException e){
+                HttpWebResponse resError=(HttpWebResponse)e.Response;//
+                StreamReader reader2 = new StreamReader(resError.GetResponseStream());
+                string rpta = reader2.ReadToEnd();
+                Assert.AreEqual("{\"Codigo\":\"ERR003\",\"Mensaje\":\"Horario ya registrado\"}", rpta);
+            }            
         }
-
 
 
         [TestMethod]

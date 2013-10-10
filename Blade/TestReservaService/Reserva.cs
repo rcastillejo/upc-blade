60aa68a86226dfd315fa6068a8fda3574ff39b13
ricardo.castillejo <ricardo.castillejo@gmail.com>
2013-10-10 03:13:35 +0000

    Se carga las fuentes del test de reserva Service

---
+++ Blade/TestReservaService/Reserva.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReservasServices.Dominio
+{
+    public class Reserva
+    {
+        public int Codigo { get; set; }
+
+        public int CodigoEspacio { get; set; }
+
+        public string Dia { get; set; }
+
+        public string FechaInicio { get; set; }
+
+        public string FechaFin { get; set; }
+
+        public int CantidadHoras{ get; set; }
+
+        public string Estado { get; set; }
+
+    }
+}
---
+++ Blade/TestReservaService/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
+using ReservasServices.Dominio;
+
+namespace TestReservaService
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CrearOk()
+        {
+            string reservaJson = "{\"CodigoEspacio\":1,\"Dia\":\"jueves\",\"FechaInicio\":\"2013/10/09 09:00:00\",\"FechaFin\":\"2013/10/09 11:00:00\",\"CantidadHoras\":2,\"Estado\":\"RESERVADO\"}";
+            byte[] data = Encoding.UTF8.GetBytes(reservaJson);
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://localhost:19528/Reservas.svc/Reservas");
+            req.Method = "POST";
+            req.ContentLength = data.Length;
+            req.ContentType = "application/json";
+
+            var reqStream = req.GetRequestStream();
+            reqStream.Write(data, 0, data.Length);
+
+            var res = (HttpWebResponse)req.GetResponse();
+            StreamReader reader = new StreamReader(res.GetResponseStream());
+            string reservaObtenidoJson = reader.ReadToEnd();
+
+            Assert.IsTrue(reservaObtenidoJson.Contains("La reserva registrada exitosamente "));
+        }
+
+        [TestMethod]
+        public void ListarOk()
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://localhost:19528/Reservas.svc/Reservas");
+            req.Method = "GET";
+
+            var res = (HttpWebResponse)req.GetResponse();
+            StreamReader reader = new StreamReader(res.GetResponseStream());
+            string resultadoObtenidoJson = reader.ReadToEnd();
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            List<Reserva> reservas = js.Deserialize<List<Reserva>>(resultadoObtenidoJson);
+
+            Assert.IsTrue(reservas.Count > 0);
+        }
+    }
+}

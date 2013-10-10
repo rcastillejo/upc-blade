5df05b01666346a3392c0e90638194dcbd3bb69c
ricardo.castillejo <ricardo.castillejo@gmail.com>
2013-10-10 23:10:43 +0000

    Se carga las fuentes de los test

---
+++ Blade/TestReservaService/Error.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestReservaService
+{
+
+    public class Error
+    {
+        public string Codigo { get; set; }
+        public string Mensaje { get; set; }
+    }
+}
--- Blade/TestReservaService/UnitTest1.cs
+++ Blade/TestReservaService/UnitTest1.cs
@@ -34,6 +34,37 @@ namespace TestReservaService
             Assert.IsTrue(reservaObtenidoJson.Contains("La reserva registrada exitosamente "));
         }
 
+
+        [TestMethod]
+        public void CrearError()
+        {
+            try{
+                string reservaJson = "{\"CodigoEspacio\":1,\"FechaInicio\":\"2013-10-14 09:00:00\",\"CantidadHoras\":2}";
+                byte[] data = Encoding.UTF8.GetBytes(reservaJson);
+
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://localhost:19528/Reservas.svc/Reservas");
+                req.Method = "POST";
+                req.ContentLength = data.Length;
+                req.ContentType = "application/json";
+
+                var reqStream = req.GetRequestStream();
+                reqStream.Write(data, 0, data.Length);
+
+                var res = (HttpWebResponse)req.GetResponse();
+                StreamReader reader = new StreamReader(res.GetResponseStream());
+                string reservaObtenidoJson = reader.ReadToEnd();
+            }
+            catch (WebException e)
+            {
+                HttpWebResponse resError = (HttpWebResponse)e.Response;//
+                StreamReader reader2 = new StreamReader(resError.GetResponseStream());
+                string resultado = reader2.ReadToEnd();
+                JavaScriptSerializer js2 = new JavaScriptSerializer();
+                Error error = js2.Deserialize<Error>(resultado);
+                Assert.AreEqual("El espacio deportivo ya ha sido reservado", error.Mensaje);
+              }
+        }
+
         [TestMethod]
         public void ListarOk()
         {

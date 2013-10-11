57ff805aa0d6ed4be4c17d3aaac13326f99002d2
zrcllspnz <zrcllspnz@425217d3-b716-0fd7-7625-4c16d5886a98>
2013-10-11 02:18:25 +0000

    Se carga el servicio de Mensajeria

---
+++ Blade/MensajeriaService/Dominio/Reserva.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace MensajeriaService.Dominio
+{
+
+    [DataContract]
+    public class Reserva
+    {
+        [DataMember]
+        public int Codigo { get; set; }
+
+        [DataMember]
+        public int CodigoEspacio { get; set; }
+
+        [DataMember]
+        public string Dia { get; set; }
+
+        [DataMember]
+        public string FechaInicio { get; set; }
+
+        [DataMember]
+        public string FechaFin { get; set; }
+
+        [DataMember]
+        public int CantidadHoras { get; set; }
+
+        [DataMember]
+        public string Estado { get; set; }
+
+    }
+}

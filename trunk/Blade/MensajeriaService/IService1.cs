c7e80ac8a83650410601efb5665ef56b6da5f51a
zrcllspnz <zrcllspnz@425217d3-b716-0fd7-7625-4c16d5886a98>
2013-10-11 02:18:25 +0000

    Se carga el servicio de Mensajeria

---
+++ trunk/Blade/MensajeriaService/IService1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using MensajeriaService.Dominio;
+
+namespace MensajeriaService
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "Mensajeria", ResponseFormat = WebMessageFormat.Json)]
+        void Grabar(Reserva horarioACrear);
+
+        [WebInvoke(Method = "GET", UriTemplate = "Mensajeria", ResponseFormat = WebMessageFormat.Json)]
+        List<Reserva> Listar();
+    }
+
+}
---
+++ trunk/Blade/MensajeriaService/Service1.svc.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Messaging;
+using MensajeriaService.Dominio;
+
+namespace MensajeriaService
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    public class Service1 : IService1
+    {
+        public void Grabar(Reserva mensaje)
+        {
+            saveQueue(mensaje);
+        }
+
+        private void saveQueue(Reserva mensaje)
+        {
+            string rutaCola = @".\private$\bladeReserva";
+            if (!MessageQueue.Exists(rutaCola))
+            {
+                MessageQueue.Create(rutaCola);
+            }
+            MessageQueue cola = new MessageQueue(rutaCola);
+            Message msg = new Message();
+            msg.Label = "Mensaje";
+            msg.Body = mensaje;
+            cola.Send(msg);
+        }
+
+        public List<Reserva> Listar()
+        {
+            return loadFromQueue();
+        }
+
+
+        private List<Reserva> loadFromQueue()
+        {
+
+            List<Reserva> reservas = new List<Reserva>();
+            string rutaCola = @".\private$\bladeReserva";
+            if (!MessageQueue.Exists(rutaCola))
+            {
+                MessageQueue.Create(rutaCola);
+            }
+
+            MessageQueue cola = new MessageQueue(rutaCola);
+            int cantidadMensajes = cola.GetAllMessages().Count();
+
+            while (cantidadMensajes > 0)
+            {
+
+                cola.Formatter = new XmlMessageFormatter(new Type[] { typeof(Reserva) });
+
+                Message mensaje = cola.Receive();
+
+                Reserva reserva = (Reserva)mensaje.Body;
+                try
+                {
+                    reservas.Add(reserva);
+                }
+                catch
+                {
+
+                }
+
+                cantidadMensajes--;
+            }
+
+            return reservas;
+
+        }
+
+    }
+}
